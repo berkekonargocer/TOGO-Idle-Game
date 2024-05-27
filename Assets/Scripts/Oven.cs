0de12329d52970538c6c9eb73fcadca414042f7f
Berke Konargöçer <berke.konargocer@gmail.com>
2024-05-27 22:26:32 +0300

    Oven.cs Created

---
+++ Assets/Scripts/Oven.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace NOJUMPO
+{
+    public class Oven : MonoBehaviour
+    {
+        // -------------------------------- FIELDS ---------------------------------
+
+
+        // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+        }
+
+        void OnEnable() {
+        }
+
+        void OnDisable() {
+        }
+
+        void Start() {
+        }
+
+        void Update() {
+        }
+
+
+        // ------------------------- CUSTOM PUBLIC METHODS -------------------------
+
+
+        // ------------------------ CUSTOM PROTECTED METHODS -----------------------
+
+
+        // ------------------------- CUSTOM PRIVATE METHODS ------------------------
+    }
+}
