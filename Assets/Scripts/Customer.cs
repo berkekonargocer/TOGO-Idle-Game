20d2e2cc73decf23ff5e08089edb5bd4a5423495
Berke Konargöçer <berke.konargocer@gmail.com>
2024-06-06 02:30:14 +0300

    CustomerStateMachine.cs and CustomerState.cs Created

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -26,17 +26,18 @@ namespace NOJUMPO
         }
 
         void Start() {
+            _customerAgent.SetDestination(buyAreaTransform.position);
         }
 
         void Update() {
-            if (isBuying && Vector3.Distance(transform.position, buyAreaTransform.position) > 0.3f)
-            {
-                _customerAgent.SetDestination(buyAreaTransform.position);
-            }
-            else if (!isBuying && Vector3.Distance(transform.position, idleAreaTransform.position) > 0.3f)
-            {
-                _customerAgent.SetDestination(idleAreaTransform.position);
-            }
+            //if (isBuying && Vector3.Distance(transform.position, buyAreaTransform.position) > 0.3f)
+            //{
+            //    _customerAgent.SetDestination(buyAreaTransform.position);
+            //}
+            //else if (!isBuying && Vector3.Distance(transform.position, idleAreaTransform.position) > 0.3f)
+            //{
+            //    _customerAgent.SetDestination(idleAreaTransform.position);
+            //}
         }
 
 
