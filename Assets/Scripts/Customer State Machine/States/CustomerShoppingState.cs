71cec9b9968f634cbd00cab8347d3464ac6a405f
Berke Konargöçer <berke.konargocer@gmail.com>
2024-06-29 20:26:15 +0300

    CustomerShoppingState.cs Wait Before Shopping and Buy Random amount of Items

--- Assets/Scripts/Customer State Machine/States/CustomerShoppingState.cs
+++ Assets/Scripts/Customer State Machine/States/CustomerShoppingState.cs
@@ -35,10 +35,23 @@ namespace NOJUMPO
                 await UniTask.Yield();
             }
 
-            shoppingStand.BreadStack.RemoveItem();
+            await UniTask.WaitForSeconds(2);
+
+            int randomAmount = Random.Range(1, 4);
+
+            while (shoppingStand.BreadStack.GetItemCount < randomAmount)
+            {
+                await UniTask.Yield();
+            }
+
+            for (int i = 0; i < randomAmount; i++)
+            {
+                shoppingStand.BreadStack.RemoveItem();
+                _audioSource.PlayOneShot(purchaseSFX);
+                Bank.Instance.PlayerAccount.AddMoney(50);
+            }
+
             GameManager.Instance.BreadCustomerQueue.RemoveFirst();
-            _audioSource.PlayOneShot(purchaseSFX);
-            Bank.Instance.PlayerAccount.AddMoney(50);
             _stateMachine.ChangeState(_stateMachine.StateFactory.Shopped);
         }
     }
